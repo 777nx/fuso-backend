7f85a7d14ba034f814586abde6007fd2331acd76
Fantasy <1362724990@qq.com>
2026-07-11 21:38:12 +0800

    feat: 后端初始化

---
+++ src/main/java/com/fantasy/fusobackend/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.fantasy.fusobackend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fantasy.fusobackend.model.entity.User;
+
+/**
+ * 用户数据库操作
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
